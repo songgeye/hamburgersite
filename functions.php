81bdc02bf540f4165801feb52feb8ffd9b377ec4
songgeye <79154673+songgeye@users.noreply.github.com>
2021-10-01 18:25:03 +0900

    deleted pagenation's function

--- functions.php
+++ functions.php
@@ -38,23 +38,3 @@
             'sidebar' => 'サイドバー',
             'footer' => 'フッター'
     ]);
-
-    // function the_pagination() {
-    //     global $wp_query;
-    //     $big = 999999999;
-    //     if ( $wp_query->max_num_pages <= 1 )
-    //         return;
-    //     echo '<nav class="pagination">';
-    //     echo paginate_links( array(
-    //         'base'         => str_replace( $big, '%#%', esc_url( get_pagenum_link($big) ) ),
-    //         'format'       => '',
-    //         'current'      => max( 1, get_query_var('paged') ),
-    //         'total'        => $wp_query->max_num_pages,
-    //         'prev_text'    => '<<',
-    //         'next_text'    => '>>',
-    //         'type'         => 'list',
-    //         'end_size'     => 1,
-    //         'mid_size'     => 1
-    //     ) );
-    //     echo '</nav>';
-    //     }
