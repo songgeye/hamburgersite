33d547e62cc5ba87aa7ede1dccd6d7c18fcb6274
songgeye <79154673+songgeye@users.noreply.github.com>
2021-10-03 21:55:38 +0900

    added single-item.php

---
+++ single-item.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?> 
+    
+            <main>
+                <div class="key_visual">
+                    <div class="key_visual__layer3">
+                        <h1><?php the_title(); ?></h1>
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                </div>
+
+                <section id="single_page">
+                    <div class="container">
+                    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+                        <div id="post-<?php the_ID(); ?>"<?php post_class(); ?>>
+                            <?php the_content(); ?>
+                            <?php wp_link_pages('before=<p>&after=</p>&next_or_number=number&pagelink= %'); ?>
+                        </div>
+                        <div class="p-menu-card__info">
+                            <p>価格：<?php $price = get_post_meta(get_the_ID(  ), '価格', true); ?><?php echo $price; ?>円</p>
+                            <p>アレルギー：<?php $allergie = get_post_meta(get_the_ID(  ), 'アレルギー', true); ?><?php echo $allergie; ?></p>
+                        </div>
+                    <?php endwhile; 
+                    else:
+                    ?>
+                    <p>表示する記事がありません</p>
+                    <?php endif; ?>
+                    </div>
+                </section>
+
+            </main>
+        </div>
+
+        <div class="flex_right">
+        <?php get_sidebar(); ?>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
